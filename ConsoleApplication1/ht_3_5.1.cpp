9c0ffa944eff3019e0810f2ad6d0c92ec3bf9f0d
Raymond <robertkejn@gmail,com>
2023-03-13 21:14:20 +0400

    Initial Commit

---
+++ ConsoleApplication1/ht_3_5.1.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <vector>
+
+template <class T>
+T quad(T input) {
+    return input * input;
+}
+
+template<>
+std::vector<int> quad(std::vector<int> input) {
+    std::vector<int> v;
+    for (std::vector<int>::iterator i = input.begin(); i != input.end(); ++i) {
+        v.push_back(*i * *i);
+    }
+    return v;
+}
+
+int main()
+{
+    std::vector<int> v = { 1,2,3,4,5,6,7,8 };
+    int a = 10;
+
+    v = quad(v);
+
+    for (auto i = v.begin(); i != v.end(); ++i) {
+        std::cout << *i << " ";
+    }
+    std::cout << "\n";
+
+    a = quad(a);
+
+    std::cout << a;
+}
